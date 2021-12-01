44de783f68c2647d949165458f998e46145b64b2
jand2421 <jaremandersen@gmail.com>
2021-11-30 23:33:21 -0700

    Behold! My shame!

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  def part1 do
+    input = AdventOfCode.Input.get!(1, 2021) |> String.split("\n")
+    IO.puts(countIncreasing(input, 1))
   end
 
-  def part2(args) do
+  def countIncreasing(list, count) do
+    [first | rest] = list
+    [second | _tail] = rest
+
+    cond do
+      length(rest) == 1 -> count
+      first < second -> countIncreasing(rest, count + 1)
+      first > second -> countIncreasing(rest, count)
+    end
+  end
+
+  def part2() do
+    input =
+      AdventOfCode.Input.get!(1, 2021)
+      |> String.split()
+      |> Enum.map(&String.to_integer(&1))
+
+    IO.puts(countIncreasing2(input, 1))
+  end
+
+  def countIncreasing2(list, count) do
+    [first | rest] = list
+    [second | rest2] = rest
+    [third | rest3] = rest2
+    [fourth | _tail] = rest3
+
+    cond do
+      length(rest) == 3 -> count
+      first + second + third < second + third + fourth -> countIncreasing2(rest, count + 1)
+      first + second + third >= second + third + fourth -> countIncreasing2(rest, count)
+    end
   end
 end
--- lib/mix/tasks/d01.p2.ex
+++ lib/mix/tasks/d01.p2.ex
@@ -8,10 +8,7 @@ defmodule Mix.Tasks.D01.P2 do
     input = nil
 
     if Enum.member?(args, "-b"),
-      do: Benchee.run(%{part_2: fn -> input |> part2() end}),
-      else:
-        input
-        |> part2()
-        |> IO.inspect(label: "Part 2 Results")
+      do: Benchee.run(%{part_2: fn -> part2 end}),
+      else: part2 |> IO.inspect(label: "Part 2 Results")
   end
 end
