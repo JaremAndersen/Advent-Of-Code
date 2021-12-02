e30eed88032dcb0501913ac9e90622ab905a0811
Jarem Andersen <jarem.andersen@pdq.com>
2021-12-02 11:17:38 -0700

    day 2 complete captain

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,21 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    {x, y} = Enum.reduce(args, {0, 0}, fn val, acc -> move(val, acc) end) |> IO.inspect()
+    x * y
   end
 
+  def move("forward " <> quantity, {x, y}), do: {x + String.to_integer(quantity), y}
+  def move("down " <> quantity, {x, y}), do: {x, y + String.to_integer(quantity)}
+  def move("up " <> quantity, {x, y}), do: {x, y - String.to_integer(quantity)}
+
   def part2(args) do
+    {x, y, _a} = Enum.reduce(args, {0, 0, 0}, fn val, acc -> move2(val, acc) end) |> IO.inspect()
+    x * y
   end
+
+  def move2("forward " <> quantity, {x, y, a}),
+    do: {x + String.to_integer(quantity), y + String.to_integer(quantity) * a, a}
+
+  def move2("down " <> quantity, {x, y, a}), do: {x, y, a + String.to_integer(quantity)}
+  def move2("up " <> quantity, {x, y, a}), do: {x, y, a - String.to_integer(quantity)}
 end
